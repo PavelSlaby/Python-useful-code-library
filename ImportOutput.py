0edad27f0b3c92188fe26c7703c1ef55196aaf42
PavelSlaby <67926620+PavelSlaby@users.noreply.github.com>
2020-10-12 21:41:35 +0200

    Update ImportOutput.py

--- ImportOutput.py
+++ ImportOutput.py
@@ -1,10 +1,9 @@
-
 import os
 os.getcwd()
+os.chdir('f:\\_Python')
 
 file = 'f:\\_Python\pokus.xlsx'
 file2 = 'f:\\_Python\pokus.txt'
-
 
 f = open(file2, "r")
 
@@ -12,13 +11,11 @@ print(f.read())
 print(f.read(5)) #specify how many characters to return
 print(f.readline()) #it iterates
 
-
 # to go through the whole file line by line
 for i in open(file2, "r"):
     print(i)
 
 f.close()
-
 
 '''
 "r" - Read - Default value. Opens a file for reading, error if the file does not exist
@@ -27,23 +24,44 @@ f.close()
 "x" - Create - Creates the specified file, returns an error if the file exists
 '''
 
-
 a = open(file2, 'a')
-a.write('just added a line')
+a.write('just added a line  ') # returns the number of signs
 a.close()
 a = open(file2, 'r')
 print(a.read())
-
 
 a = open(file2, 'w')
 a.write('just rewrote the file')
 
 a = open('created_file.txt', 'x')
 
-import os
-
-os.remove('crated_file.txt')
+a.close() # file has to be closed before being removed
+os.remove('created_file.txt')
 
 os.path.exists(file)
 
 os.rmdir() # removes a directory
+
+print(f.read())
+
+w = open("Test.txt", 'w')
+w.write("ahoj")
+w.close()
+
+
+f1 = open("Test.txt", 'w')
+for i in range(10):
+    f1.writelines(str(i) + ' ' + str(i ** 2) + '\n')
+        
+f1.close()    
+
+if os.path.exists("Test.txt"):
+    os.remove("Test.txt")
+
+os.mkdir("WorkingFiles")
+os.rmdir("WorkingFiles")
+
+
+
+
+
