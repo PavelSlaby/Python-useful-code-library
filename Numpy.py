74bfec96a4dba6133ae4caa4106dc1326b6d3a93
PavelSlaby <67926620+PavelSlaby@users.noreply.github.com>
2022-01-03 17:24:58 +0100

    Update Numpy.py

--- Numpy.py
+++ Numpy.py
@@ -1,4 +1,4 @@
-## Arrays
+ ## Arrays
 v = [0.5, 0.75, 1.0, 1.5, 2.0]
 w = [5, 75, 1, 1, 2]
 u = [3, 4, 5, 6, 7]
@@ -6,20 +6,18 @@ t = [0, 0, 0, 0, 0]
 
 m = [v, w, u, t]
 m
-print(m)
+
+v[0] = 10
+m #changes as well. one solution is to use deepcopy module
 
 m[1]
 m[1][1]
-
-#there is python array thingy
-import array
-array.array('f',[1, 2, 3]) 
-#this is not very convenient
 
 import numpy as np
 np.__version__
 
 a = np.array([0, 0.5, 10 , 0])
+
 type(a)
 a.dtype
 
@@ -30,7 +28,8 @@ a[::2]
 a[1:4:2]
 a[:2]
 a * 2
-a.sum()
+
+a.sum() # these math operations are in general slower using numpy than math module
 np.sum(a)
 a.std()
 a.cumsum()
@@ -42,27 +41,30 @@ np.mean(a)
 a.argmin()
 a.argmax()
 
+b.sum(axis=0) #sum columnwise - same as pandas
+b.sum(axis=1) #sum rownwise - same as pandas
+b.sum() == b.sum(axis = None)
+
 b = np.array([a, a * 2])
-print(a)
 print(b)
 
 b[0][1]
 b[1][2]
 b[1, 2] #same as b[1][2]
 
-b.sum(axis=0) #sum columnwise - same as pundas
-b.sum(axis=1) #sum rownwise - sae as pandas
+b[0,2] == b[0][2] #!!!
 
-u = np.array([0, 0.5, 10 , 0], dtype = 'S') #string type
-print(u)
-u = u.astype('f') # change type
-
-d = np.zeros((5, 2, 3))
-d
+np.zeros((5, 2, 3))
+np.zeros([5, 2, 3]) #works sboth using brackets and parentheses
 
 e = np.ones([2,2])
-e
 a = list(e)
+a
+
+np.zeros_like(e)
+np.ones_like(e)
+np.eye(5)
+np.linspace(1, 10, 20)
 
 d3 = np.array([[[1, 2], [1, 2]], [[3, 4], [3, 4]], [[3, 4], [3, 4]], [[3, 4], [3, 4]], [[3, 4], [3, 4]]])
 d3
@@ -94,6 +96,10 @@ np.pi
 ar1 = np.array([[1, 2, 3], [10, 20, 30]])
 ar2 = np.array([[4, 5, 6], ['a', 20, 30]])
 
+ar1.size
+ar1.shape
+np.shape(ar1)
+
 ar = np.concatenate((ar1, ar2))
 ar
 ar = np.concatenate((ar1, ar2), axis = 1) #equivalent to hstack
@@ -117,7 +123,7 @@ cr  = np.array_split(br, 8)
 cr
 
 o = np.array(np.vstack([range(10), range(10)]))
-print(np.where(o == 3))
+np.where(o == 3)
 o
 np.sort([10, 4, 5, 6])
 p = np.array(range(10))
@@ -128,14 +134,12 @@ s
 r = np.array([[0, 0], [4, 4]])
 r, s
 r + s
-r * s #elemtwise
+r * s #elementwise
 r**2
 r.dot(s)  # "classical" multiplication of two arrays
-r.dot?
-np.shape(r)
-np.shape(r.T)
-r.transpose()
+
 r.T
+r.transpose()
 2 * r + 3
 
 ar = np.array(range(12))
@@ -148,16 +152,43 @@ ar5 = ar4.reshape(-1)  #flattening the array
 ar5
 ar5[ar5 > 2] = 10
 
-np.zeros((3,2))
-x = np.eye(9)
-x
-c = np.diag(x)
-c
-
-help(np.diag)
 np.diag([7,5,6])
+
+np.resize(b, (2, 2)) # takes first elements, ignores rest
+np.resize(b, (3, 3)) # repeats elements
+
+np.ones(10).apply(lambda x: x **2) # does not work on numpy
+list(map(lambda x: x + 89, np.ones(10))) # map works on numpy
+
+np.arange(8, dtype = 'float')
+u = np.array([0, 0.5, 10 , 0], dtype = 'S') #string type
+print(u)
+u = u.astype('f') # change type
+
+a = np.zeros((4,2), dtype = 'int') #when type is specified, can not assign any other type in future, 
+a[0, 0] = '8.5'
+
+a = np.zeros((4,2)) #type is not specified...
+a[0, 0] = '8.5'
+
+b.flatten(order = 'C') #row by row
+b.flatten(order = 'F') #column by column
+b.flatten() #default is row by row
+
+(b >= 3) & (b< 5).astype(int)
+
+b == 1
+np.where(b==2, 'pravda',b/2 + 89)
+
+def f(x):
+    return x + 3
+
+f(3)
+f(np.array([2, 2, 3]))
+
+
+
 #%%
-import numpy as np
 from numpy import random
 
 random.randint(100) #generate random integer from  0 to 100 (not including 100 but includig 0)
@@ -165,9 +196,10 @@ np.random.rand() #random float between 0 and 1
 
 # random arrays
 random.randint(100, size = (2,2))
+random.randint(100, size = (200))
 random.rand(2, 2) #random float between 0 and 1
-random.choice([1, 2, 3, 10, 5], size = (10, 10))
 
+random.choice([1, 2, 3, 10, 5], size = (10, 10))
 random.choice([1, 2, 3], p= [0.3, 0.4, 0.3], size = (3))
 
 # randomly reschuffle and array
@@ -178,10 +210,10 @@ y
 random.permutation(y) #same as shuffle, but leaves the original array intact
 
 I = 5000
-%time mat2 = np.random.standard_normal((I , I))
-%time mat2.sum()
-%timeit y.sum()
+np.random.standard_normal((I , I))
 
+
+#%%
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -220,7 +252,7 @@ random.logistic(loc = 1, scale =2, size = 1000)
 sns.distplot(random.normal(loc = 0, scale =1, size = 1000), hist = False, label = 'Normal')
 sns.distplot(random.logistic(loc = 0, scale =1, size = 1000), hist= False, label = 'logistic')
 sns.distplot(random.lognormal(mean = 0.0, sigma = 1, size = 1000)) # can not be negative
-# ligistic distribution has more area in the tails, atherwise the distributions are similar
+# logistic distribution has more area in the tails, atherwise the distributions are similar
 
 #%% exponential dist
 
@@ -299,4 +331,46 @@ np.unique([2, 3, 2]) #unique
 np.intersect1d([2, 3], [2])
 np.setdiff1d(a, d)
 np.setxor1d(a, d)  #values that are NOT present in BOTH sets      
-        
+
+#%% Numpy random module
+
+np.set_printoptions(precision = 8)  
+np.random.seed(1000)
+
+np.random.rand(2, 2) #random values
+np.random.randn(10) #standard normal distribution
+np.random.randint(1, 3, 5) #not including the second parameter
+np.random.ranf(5) #random floats
+np.random.choice((1,2), 5)
+
+#%%
+
+import sys
+np.set_printoptions(threshold=sys.maxsize) # sets to print maximum size  
+np.set_printoptions(threshold=50) # sets to print maximum size              
+
+np.set_printoptions(edgeitems=30, linewidth=100000, formatter=dict(float=lambda x: "%.3g" % x))
+
+#%% statistics
+y = [ 8. ,  1. ,  2.5,  4. , 28. ]
+w = [0.1, 0.5, 0.1, 0.1, 0.2]
+np.average(y, weights = w)
+
+np.var(y, ddof=1) #degrees of freedom (n-1)
+np.var(y)  #var varies based on DoF!!!!!!!!!
+
+import statistics
+statistics.stdev(y)
+np.std(y, ddof = 1)
+np.std(y) #std varies based on DoF!!!!!!!!!
+np.std?
+
+np.percentile(x, 95)
+
+np.cov(y, w)
+np.corrcoef(w, y)
+
+#%%
+
+%precision 1
+np.set_printoptions(suppress=True) # supresses scientific notations when printing numpy arrays
