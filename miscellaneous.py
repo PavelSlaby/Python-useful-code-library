258186ff2781a9945d72f8b95073acfef39dfb6c
pavel <pavel.slaby@seznam.cz>
2026-02-28 14:37:35 +0100

    renaming

---
+++ miscellaneous.py
@@ -0,0 +1,15 @@
+''''
+ALT + Enter         - to preview warnings and apply a quick fixes
+shift twice         - search anywhere
+ctrl + /            - to comment
+ctrl + -            - to collapse a piece of code
+shift + F6          - to rename
+ctr + alt + L       - to format the coee
+cr shift r          - to replace
+'''
+
+'''
+CTRL-SHIFT-ESC   -- to bring up the task manager
+folder/filenames are not case sensitive on Windows and macOS
+
+'''
---
+++ python_general.py
@@ -0,0 +1,55 @@
+
+'''
+- Python uses
+    - multiparadigm approach -can write code in procedural, object-oriented, functional or imperative manner
+    - interpreted
+    - Dynamically typed - Types in Python are in general inferred at runtime and not statically declared
+
+
+- Indentation:
+    - The number of spaces is up to you as a programmer, but it has to be at least one. Best practice is to use 4spaces instead of tab.
+
+- PEP 8:
+    - a recommendation for a "default" style exists for Python, the PEP 8 (PEP = Python Enchancement Proposal): https://www.python.org/dev/peps/pep-0008/
+
+    Naming Conventions:
+        - Modules: all lowercase names
+        - Class names: PascalCase
+        - Constant variables: SNAKE_CASE
+        - function, methods, variables: lowercase snake_case
+
+'''
+
+import this  # The Zen of Python
+
+
+#following might not work in all IDEs - ony in IPython
+%magic  # all available magic commands, with description
+%lsmagic  # short list of the previous
+
+# examples of magic commands:
+%clear
+%history
+%timeit
+
+%time
+4 + 4  # Time execution of a Python statement or expression.
+%%python?
+%quickref  # Quick reference of all IPython specific syntax and magics.
+
+import keyword
+keyword.kwlist  # list of all current reserved keywords in python
+
+?  # -> Introduction and overview of IPython's features.
+help()  # Access Python's own help system.
+help()  # specific help
+
+pip
+list  # list all the packages installed on your system:
+
+
+print(keyword.__doc__) #  Gets docstring from a module/class/function
+help(keyword)
+
+
+print(input("vek:"))
